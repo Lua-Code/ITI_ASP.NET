951a0a2a272592bbf64f2972be07b780c7ce7b2d
Kevin David <kevindavidvenyk@gmail.com>
2026-09-12 10:36:22 +0300

    Add project files.

---
+++ ITI_ASP.NET/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ITI_ASP.NET.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+        public string Brand { get; set; }
+        public int Stock { get; set; }
+        public double Rating { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
